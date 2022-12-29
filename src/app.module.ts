395df7aedf089a8596c95abe7083c30562c2d4a2
master <mastersu93@gmail.com>
2022-12-29 13:09:37 +0300

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,17 @@ import { UsersModule } from './users/users.module';
 import { ImagesModule } from './images/images.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import * as process from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     ImagesModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://master:Derbent_5000@cluster0.0zoww.mongodb.net/?retryWrites=true&w=majority',
-      {
-        dbName: 'dioraKids',
-      },
-    ),
-    // MongooseModule.forRoot('mongodb://localhost/authentication'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      dbName: process.env.MONGODB_NAME,
+    }),
     AuthModule,
   ],
 })
